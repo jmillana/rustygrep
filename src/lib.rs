0dbd85841a82ac64aac0660e46e55cafd09f9d0b
jmillana <jordi@marimon.tech>
2023-04-06 18:42:53 +0200

    Read args from cmd
    Implement the logic to read arguments from cmd
    Implement a struct to represent the arguments
    Implement a basic run method that reads a given file

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        /* As the `new` method is expected to never fail, we build a
+           `build` method that returns a `Result`
+        */
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{}", contents);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// use super::lib::run;
+use rustygrep::run;
+use rustygrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
